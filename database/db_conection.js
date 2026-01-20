a8fc7a11789fa773e78e83ef4f5385a335b5a99a
johngalarza <johngalarza1970@gmail.com>
2026-01-20 07:33:12 -0500

    fixed: Conecction to database

--- database/db_conection.js
+++ database/db_conection.js
@@ -1,9 +1,15 @@
-const { Sequelize, DataTypes } = require('sequelize');
+import { Sequelize, DataTypes } from 'sequelize';
 import 'dotenv/config';
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
     dialect: process.env.DB_USE,
-    port: process.env.DB_PORT
-});
+    port: process.env.DB_PORT,
+  }
+);
 
-module.exports = { DataTypes, sequelize };
+export { sequelize, DataTypes };
