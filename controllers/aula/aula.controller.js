60e63bec9990fc385ba3032639dee4130baad917
johngalarza <johngalarza1970@gmail.com>
2026-02-07 18:05:28 -0500

    fixed: get available aulas

--- controllers/aula/aula.controller.js
+++ controllers/aula/aula.controller.js
@@ -1,5 +1,6 @@
 const Aula = require('../../models/Aula');
 const Horario = require('../../models/Horario');
+const { Op } = require('sequelize');
 
 exports.getAll = async (req, res) => {
   try {
@@ -12,16 +13,43 @@ exports.getAll = async (req, res) => {
 
 exports.getDisponibles = async (req, res) => {
   try {
-    const aulas = await Aula.findAll({
-      include: {
-        model: Horario,
-        required: false,
+    const now = new Date();
+
+    const dias = [
+      'DOMINGO',
+      'LUNES',
+      'MARTES',
+      'MIERCOLES',
+      'JUEVES',
+      'VIERNES',
+      'SABADO'
+    ];
+
+    const diaActual = dias[now.getDay()];
+    const horaActual = now.toTimeString().slice(0, 8);
+
+    const aulasOcupadas = await Horario.findAll({
+      attributes: ['id_aula'],
+      where: {
+        dia: diaActual,
+        id_aula: { [Op.ne]: null },
+        hora_inicio: { [Op.lte]: horaActual },
+        hora_fin: { [Op.gt]: horaActual }
+      },
+      group: ['id_aula']
+    });
+
+    const idsOcupadas = aulasOcupadas.map(h => h.id_aula);
+
+    const aulasDisponibles = await Aula.findAll({
+      where: {
+        id_aula: idsOcupadas.length
+          ? { [Op.notIn]: idsOcupadas }
+          : { [Op.ne]: null }
       }
     });
 
-    // lógica simple: si no tiene horario → disponible
-    const disponibles = aulas.filter(a => a.Horarios?.length === 0);
-    res.json(disponibles);
+    res.json(aulasDisponibles);;
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
