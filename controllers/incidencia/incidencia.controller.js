20ba779e385201494dc563e6b7f005483140486b
johngalarza <johngalarza1970@gmail.com>
2026-02-11 15:24:49 -0500

    feat: Routes of count reports

--- controllers/incidencia/incidencia.controller.js
+++ controllers/incidencia/incidencia.controller.js
@@ -1,8 +1,11 @@
 const { Incidencia } = require('../../models');
+const { Op } = require('sequelize');
 
 exports.create = async (req, res) => {
   try {
-    const {tipo, descripcion, estado, id_usuario, id_aula, id_laboratorio, id_equipo} = req.body;
+    const {tipo, descripcion, estado, id_aula, id_laboratorio, id_equipo} = req.body;
+    const id_usuario = req.user.id_usuario;
+
     const result = await Incidencia.create({tipo, descripcion, estado, id_usuario, id_aula, id_laboratorio, id_equipo});
     res.status(201).json(result);
   } catch (error) {
@@ -10,13 +13,37 @@ exports.create = async (req, res) => {
   }
 };
 
-exports.getReporte = async (req, res) => {
+exports.getAulaCount = async (req, res) => {
   try {
-    const reporte = await Incidencia.findAll({
-      attributes: ['id_aula', 'id_laboratorio', [Incidencia.sequelize.fn('COUNT', '*'), 'total']],
-      group: ['id_aula', 'id_laboratorio']
+
+    const aulas = await Incidencia.count({
+      col: 'id_aula',
+      where: {
+        id_aula: { [Op.ne]: null }
+      },
+      group: ['id_aula']
     });
-    res.json(reporte);
+
+    res.json(aulas);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getLabCount = async (req, res) => {
+  try {
+
+    const laboratorios = await Incidencia.count({
+      col: 'id_laboratorio',
+      where: {
+        id_laboratorio: { [Op.ne]: null }
+      },
+      group: ['id_laboratorio']
+    });
+
+    res.json(laboratorios);
+
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- routes/incidencia/incidencia.routes.js
+++ routes/incidencia/incidencia.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middlewares/auth.middleware');
 const incidenciaController = require('../../controllers/incidencia/incidencia.controller');
 
-router.post('/', incidenciaController.create);
-router.get('/reporte', incidenciaController.getReporte);
+router.post('/', auth, incidenciaController.create);
+router.get('/aula/count', incidenciaController.getAulaCount);
+router.get('/laboratorio/count', incidenciaController.getLabCount);
 router.get('/criticas', incidenciaController.getCriticas);
 
 module.exports = router;
